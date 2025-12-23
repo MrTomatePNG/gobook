59b02dab4ede9c9eaa5e7f65bd4dc494ca791868
MrTom9te <gabriel_santarem@outlook.com>
2025-12-23 15:34:32 -0400

    Print Celsius conversion and fix formatting

--- ch2/cf/main.go
+++ ch2/cf/main.go
@@ -4,18 +4,18 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-
 )
 
 func main() {
 
-	for _ , arg := range os.Args[1:]{
-		t, err := strconv.ParseFloat(arg,64)
-		if err != nil{
-			fmt.Fprintf(os.Stderr,"cf:%v\n",err)
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf:%v\n", err)
 		}
 
-		f :=
+		f := (t - 32) * 5 / 9
+		fmt.Printf("%g°F = %g°C\n", t, f)
 	}
 
 }
