a2f60f046a5bacad5564236095be428c0d8620c3
MrTom9te <gabriel_santarem@outlook.com>
2025-12-23 15:32:33 -0400

    Add slice reverse and equal functions

---
+++ ch4/rev/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func main() {
+	a := [...]int{1, 2, 3, 4, 5}
+	reverse(a[:])
+	fmt.Println(a)
+}
+
+func equal(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return true
+}
